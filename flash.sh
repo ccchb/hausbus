047f9f446a78a2cb03e563f204427721f4c9ff87
Tristan Bruns <tristan+git@tristanbruns.de>
2014-08-30 14:41:54 +0200

    zzzzZZZZZzzzzzzzzZZZZZZZZZZ

--- flash.sh
+++ flash.sh
@@ -2,5 +2,5 @@
 
 CCOPTS="-Wall -Wextra -Werror"
 
-avr-gcc $CCOPTS -std=c11 -Os -g -mmcu=attiny2313 -o "$1.elf" "$1.c"
-sudo avrdude -p t2313 -c usbasp -U lfuse:w:0xfd:m -U hfuse:w:0xdf:m -U efuse:w:0xff:m -U "$1.elf"
+#avr-gcc $CCOPTS -std=c11 -Os -g -mmcu=attiny2313 -o "$1.elf" "$1.c"
+avrdude -p t2313 -c usbasp -U "$1.elf"
--- slave/slave.c
+++ slave/slave.c
@@ -1,13 +1,63 @@
+#define PIN_RED 2
+#define PIN_BLUE 0
+#define PIN_GREEN 4
+#define PIN_STATUS 6
 #define F_CPU 3686400
-#define UART_BAUDRATE 9600
+#define BAUD 9600
+
 #include <avr/io.h>
+#include <util/delay.h>
+#include <util/setbaud.h>
 #include <avr/interrupt.h>
 #include "proto.h"
 
+#define bit_set(byte, bit) do{ byte |= (uint8_t)(1 << bit); }while(0)
+#define bit_clear(byte, bit) do{ byte &= (uint8_t)~(1 << bit); }while(0)
+
+static uint8_t pwm_counter;
+static uint8_t pwm_red = 0x10;
+static uint8_t pwm_blue = 0x10;
+static uint8_t pwm_green = 0x10;
+
+static uint8_t recv_counter = 0;
+
+ISR(TIMER0_COMPA_vect)
+{
+	if (pwm_counter < pwm_red)
+		bit_set(PORTD, PIN_RED);
+	else
+		bit_clear(PORTD, PIN_RED);
+
+	if (pwm_counter < pwm_blue)
+		bit_set(PORTD, PIN_BLUE);
+	else
+		bit_clear(PORTD, PIN_BLUE);
+
+	if (pwm_counter < pwm_green)
+		bit_set(PORTD, PIN_GREEN);
+	else
+		bit_clear(PORTD, PIN_GREEN);
+
+	pwm_counter++;
+}
+
 static void handle_byte(uint8_t byte) {
-	(void) byte;
-	// toggle the status led on any payload
-	PORTD ^= 1 << 6;
+	PORTD ^= 1 << PIN_STATUS;
+
+	switch (recv_counter) {
+		case 0:
+			pwm_red = byte;
+			break;
+		case 1:
+			pwm_green = byte;
+			break;
+		case 2:
+			pwm_blue = byte;
+			break;
+	}
+
+	if (++recv_counter > 2)
+		recv_counter = 0;
 }
 
 ISR(USART_RX_vect) {
@@ -16,18 +66,39 @@ ISR(USART_RX_vect) {
 }
 
 int main(void) {
-	// status LED
-	DDRD = 1 << 6;
+	// Configure output pins
+	DDRD = (1 << PIN_STATUS) | (1 << PIN_RED) | (1 << PIN_BLUE) | (1 << PIN_GREEN);
 
-	// pd0: rx
-	//// set baudrate
-	int baud = F_CPU / (UART_BAUDRATE * 16L) - 1;
-	UBRRH = baud >> 8;
-	UBRRL = baud;
-	//// set interrupt
+	// Set up timer for PWM
+	TCCR0A = (1<<WGM01); // ctc mode
+	TCCR0B = (1<<CS00); // no prescaler
+	OCR0A = 240; // set interval (200Hz)
+	TIMSK = (1<<OCIE0A); // enable interrupts
+	TIFR = (1<<OCF0A); // clear pending interrupts
+
+	// Set Baudrate
+	// see: http://www.nongnu.org/avr-libc/user-manual/group__util__setbaud.html
+	UBRRH = UBRRH_VALUE;
+	UBRRL = UBRRL_VALUE;
+#if USE_2X
+	UCSRA |= (1 << U2X);
+#else
+	UCSRA &= ~(1 << U2X);
+#endif
+
+	// Configure UART
 	UCSRB = (1 << RXEN) | (1 << RXCIE);
+
+	// Enable Interrupts
 	sei();
 
-	while(1) {
+	// Stupid main loop
+	// This basically tells us if we need a lot of time in one of the ISRs
+	// (probably useless...)
+	for (;;) {
+		//bit_set(PORTD, PIN_STATUS);
+		//_delay_ms(100);
+		//bit_clear(PORTD, PIN_STATUS);
+		//_delay_ms(100);
 	}
 }
